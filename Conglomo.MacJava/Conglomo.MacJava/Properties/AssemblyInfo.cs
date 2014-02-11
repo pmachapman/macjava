f1307c3f97793be6383a38f850370ec911da2557
pmachapman_cp <SND\pmachapman_cp>
2014-02-10 20:48:09 -0800

    Initial commit.

---
+++ Conglomo.MacJava/Conglomo.MacJava/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Peter Chapman">
+// Copyright 2014 Peter Chapman. See http://macjava.codeplex.com/license for licence details.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MacJava")]
+[assembly: AssemblyDescription("JavaScript extension for ASP.NET MVC")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Peter Chapman")]
+[assembly: AssemblyProduct("MacJava")]
+[assembly: AssemblyCopyright("Copyright 2014 Peter Chapman. See http://macjava.codeplex.com/license for licence details.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6a5d5eb7-15cc-450f-84d4-af595614cb6e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-NZ")]
+[assembly: CLSCompliant(true)]
