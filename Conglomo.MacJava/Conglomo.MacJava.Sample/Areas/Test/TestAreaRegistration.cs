e75772173003b7c7859d104cc3ae96e51f9577d7
Peter Chapman <peter@efinity.co.nz>
2014-03-28 20:05:43 +1300

    Missed half of the commit!

---
+++ Conglomo.MacJava/Conglomo.MacJava.Sample/Areas/Test/TestAreaRegistration.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TestAreaRegistration.cs" company="Peter Chapman">
+// Copyright 2014 Peter Chapman. See http://macjava.codeplex.com/license for licence details.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Conglomo.MacJava.Sample.Areas.Test
+{
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// The test area registration.
+    /// </summary>
+    public class TestAreaRegistration : AreaRegistration 
+    {
+        /// <summary>
+        /// Gets the name of the area to register.
+        /// </summary>
+        /// <returns>The name of the area to register.</returns>
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Test";
+            }
+        }
+
+        /// <summary>
+        /// Registers an area in an ASP.NET MVC application using the specified area's context information.
+        /// </summary>
+        /// <param name="context">Encapsulates the information that is required in order to register the area.</param>
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Test_default",
+                "Test/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
