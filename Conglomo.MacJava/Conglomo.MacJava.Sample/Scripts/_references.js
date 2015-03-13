ae821428f79506dcf776f1820c123bb78a7378b8
Peter Chapman <peter@conglomo.co.nz>
2015-03-13 16:58:14 +1300

    Package upgrades

--- Conglomo.MacJava/Conglomo.MacJava.Sample/Scripts/_references.js
+++ Conglomo.MacJava/Conglomo.MacJava.Sample/Scripts/_references.js
@@ -1,6 +1,4 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="jquery-2.1.1.js" />
-/// <reference path="jquery-1.10.2.js" />
+/// <reference path="jquery-2.1.3.js" />
 /// <reference path="Script.js" />
-/// <reference path="npm.js" />
 /// <reference path="bootstrap.min.js" />
