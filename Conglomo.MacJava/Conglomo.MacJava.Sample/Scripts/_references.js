ab1fb157789b6d7fa4d85157d413b57429e81bb8
Peter Chapman <peter@conglomo.co.nz>
2015-08-20 21:48:17 +1200

    Upgraded Bootstrap and jQuery.

--- Conglomo.MacJava/Conglomo.MacJava.Sample/Scripts/_references.js
+++ Conglomo.MacJava/Conglomo.MacJava.Sample/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="jquery-2.1.3.js" />
+﻿/// <reference path="jquery-2.1.4.js" />
+/// <autosync enabled="true" />
 /// <reference path="Script.js" />
 /// <reference path="bootstrap.min.js" />
